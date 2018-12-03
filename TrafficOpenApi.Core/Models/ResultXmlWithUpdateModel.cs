892bffb9b0d5dd6e73bbad15a5219266a82001e0
LightEach <lighteach@gmail.com>
2018-12-03 22:40:27 +0900

    미쳐 올라가지 못한 파일 업로드

---
+++ TrafficOpenApi.Core/Models/ResultXmlWithUpdateModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficOpenApi.Core.Models
+{
+	public enum ResultXmlWithUpdateErrorKind
+	{
+		None = 0
+		, RsXml = 1
+		, Empty = 2
+	}
+
+	public class ResultXmlWithUpdateModel
+	{
+		public ResultXmlWithUpdateModel() { }
+
+		public ResultXmlWithUpdateModel(bool success, ResultXmlWithUpdateErrorKind kind, string text)
+		{
+			IsSuccess = success;
+			WhatError = kind;
+			ResultText = text;
+		}
+
+		public bool IsSuccess { get; set; } = false;
+		public ResultXmlWithUpdateErrorKind WhatError { get; set; }
+		public string ResultText { get; set; }
+	}
+}
